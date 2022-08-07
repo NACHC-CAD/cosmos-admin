86ec010586381fc7f7d003cf8608208245246375
greshje <greshje@gmail.com>
2022-08-07 08:30:35 -0400

    work in progress

--- src/main/java/org/nachc/cosmos/web/util/env/SetEnvToDev.java
+++ src/main/java/org/nachc/cosmos/web/util/env/SetEnvToDev.java
@@ -1,18 +1,16 @@
-package integration.cosmosadmin.curlew.env;
+package org.nachc.cosmos.web.util.env;
 
-import org.junit.Test;
 import org.nachc.cad.cosmos.action.create.protocol.raw.manual.tools.enviornment.SET_ENV_TO_DEV;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class SetEnvToDevManualTest {
+public class SetEnvToDev {
 
-	@Test
-	public void shouldSetEnvToDev() {
+	public static void main(String[] args) {
 		log.info("Setting env to dev...");
 		SET_ENV_TO_DEV.main(null);
-		log.info("Done.");
+		log.info("Done.");		
 	}
 	
 }
